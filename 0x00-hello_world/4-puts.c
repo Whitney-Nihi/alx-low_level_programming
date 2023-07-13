2c838d61d9479d1c5a010520f09bf27c1efa427e
Whitney Nihi <whitney.taire1@gmail.com>
2023-07-13 15:47:15 -0700

    Created script to print out text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char text[200] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(text);
+	return (0);
+}
