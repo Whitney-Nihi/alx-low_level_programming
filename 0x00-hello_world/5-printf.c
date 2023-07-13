e1f3c0aa29c7f71f4afd33ebf6e662f870df41f9
Whitney Nihi <whitney.taire1@gmail.com>
2023-07-13 16:10:02 -0700

    created c file to print text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char text[200] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s", text);
+	return (0);
+}
