c9622162e67c23c88827a79c0ee1c531e2b66433
Whitney Nihi <whitney.taire1@gmail.com>
2023-07-13 18:04:38 -0700

    Created c program that prints text to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text to standard error
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - ");
+	fprintf(stderr, "Dora Korpar, 2015-10-19\n");
+	return (1);
+}
