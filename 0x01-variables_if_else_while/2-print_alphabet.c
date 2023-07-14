f049e151d050bf63451c8642a9bd03b4dea0de80
Whitney Nihi <whitney.taire1@gmail.com>
2023-07-14 13:37:57 -0700

    Created program that prints a -z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - prints all lower case letters
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char lower;
+
+	/* for loop to read from a - z*/
+	for (lower = 'a'; lower <= 'z'; lower++)
+	putchar(lower);
+
+	return (0);
+
+}
