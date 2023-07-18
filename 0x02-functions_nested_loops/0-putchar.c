042769296bae071f1a29fa669f20a3642b2543b3
Whitney Nihi <whitney.taire1@gmail.com>
2023-07-18 16:20:02 -0700

    Created new README and program to print a text

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*function declaration*/
+int _putchar(char c);
+/**
+ * main - Prints function - _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+README for my C programs - Functions and Nested Loops
