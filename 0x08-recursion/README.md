c15bcdf04846929beb9ccf47375f803d88fae0c8
Whitney Nihi <whitney.taire1@gmail.com>
2023-08-02 15:10:23 -0700

    Created function that prints a string and created README file to save functions descriptions

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+Functions for 0x08-recursion project
+
+-0-puts_recursion.c - Function prints a string followed by a new line
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_READER_FILE
+#define MAIN_READER_FILE
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
