0eac00c2e972a8572e23a7d6278c85269494e6ff
Whitney Nihi <whitney.taire1@gmail.com>
2023-08-11 14:05:00 -0700

    Created function that allocates memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to memory to fill
+ * @b: char to copy
+ * @n: copy frequency
+ *
+ * Return: pointer to the memory area to be filled
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: array size
+ * @size: array element size
+ *
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -2,3 +2,4 @@ README for 0x0C. C - More malloc, free project
 
 -void *malloc_checked(unsigned int b) - Function allocates memory using malloc
 -char *string_nconcat(char *s1, char *s2, unsigned int n) - Function concatenates two strings
+-void *_calloc(unsigned int nmemb, unsigned int size) - function allocates memory for an array, using malloc
