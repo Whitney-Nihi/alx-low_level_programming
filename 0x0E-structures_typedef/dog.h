de1668a94b24c0e71f6bf541caf0c8966a0a7ed5
Whitney Nihi <whitney.taire1@gmail.com>
2023-08-14 11:37:09 -0700

    Created a new type struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - struct to declare elements for dog object
+ * @name: element 1
+ * @age: element 2
+ * @owner: element 3
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
