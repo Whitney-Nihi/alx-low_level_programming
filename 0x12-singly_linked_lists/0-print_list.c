01fd66d1bc3e4d733b49bf2b9a64a14e549d346b
Whitney-Nihi <whitney.taire1@gmail.com>
2023-08-25 15:05:12 -0700

    Created a function that prints the elements in a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: head pointer
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n;
+
+	for (n = 0; h; n++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	}
+
+	return (n);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+README for 0x12. C - Singly linked lists project containing functins and their descriptions
+
+-size_t print_list(const list_t *h) - Prototype for function that prints all the elements of a linked list
