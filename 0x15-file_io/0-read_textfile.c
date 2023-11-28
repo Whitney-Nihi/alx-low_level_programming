c05ebfeff278a42d2b9a9195d23c6605ae916b4d
Whitney-Nihi <whitney.taire1@gmail.com>
2023-11-28 06:34:18 -0800

    Modified function to read files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,22 +1,31 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * read_textfile - reads a text file and prints it to POSIX stdout
- * @filename - string to store read file
- * @letters - number of letters to read and print
+ * @filename: string to store read file
+ * @letters: number of letters to read and print
  *
  * Return: 0 if filename is null or cannot be opened or read
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *ptr = NULL;
+	char *ptr;
+	ssize_t fd;
+	ssize_t i;
+	ssize_t j;
 
-	ptr = fopen("Requiescat", "r");
-	if (ptr == NULL)
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
-	fgets(filename, letters, ptr);
-		printf("%s", filename);
-		return (0);
+	ptr = malloc(sizeof(char) * letters);
+	j = read(fd, ptr, letters);
+	i = write(STDOUT_FILENO, ptr, j);
+
+	free(ptr);
+	close(fd);
+	return (i);
 }
